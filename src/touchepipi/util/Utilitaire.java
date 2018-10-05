89ac13c2dc91ed04d167b61dd160561f15f82c2c
pauld <carotte@outlook.com>
2018-10-05 15:02:56 +0200

    C'est presque finit mais pas trop parce que Bastien travail mais pas trop et Amélie est une gentille fille.

--- src/touchepipi/util/Utilitaire.java
+++
@@ -1,9 +0,0 @@
-package touchepipi.util;
-
-public class Utilitaire
-{
-    public static boolean estDiagonale(int x1,int x2, int  y1, int y2)
-    {
-        return(x1 != x2 && y1 != y2);
-    }
-}
